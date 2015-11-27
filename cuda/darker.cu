eadf903d34e2cef1d59f96667e4ed30190c9fbc6
Laurens Verspeek <laurens_verspeek@hotmail.com>
2015-11-27 10:56:21 +0100

    optimized calculations in kernel

--- cuda/darker.cu
+++ cuda/darker.cu
@@ -11,43 +11,47 @@ using std::fixed;
 using std::setprecision;
 
 // Device code
-__global__ void darkGray_kernel(const int width, const int height, const unsigned char * inputImage, unsigned char * darkGrayImage) {
+__global__ void darkGray_kernel(const int width, const int height, const int size, const unsigned char * inputImage, unsigned char * darkGrayImage) {
 	// get position of thread in the 'thread matrix'
 	const int x = blockDim.x * blockIdx.x + threadIdx.x;
 	const int y = blockDim.y * blockIdx.y + threadIdx.y;
+	const int pos = (y * width) + x;
 
 	// Check if we are inside the bounds of the image
 	if (x < width && y < height){
 		float grayPix = 0.0f;
-		float r = static_cast< float >(inputImage[(y * width) + x]);
-		float g = static_cast< float >(inputImage[(width * height) + (y * width) + x]);
-		float b = static_cast< float >(inputImage[(2 * width * height) + (y * width) + x]);
+		float r = static_cast< float >(inputImage[pos]);
+		float g = static_cast< float >(inputImage[size + pos]);
+		float b = static_cast< float >(inputImage[(2 * size) + pos]);
 
 		grayPix = ((0.3f * r) + (0.59f * g) + (0.11f * b));
 		grayPix = (grayPix * 0.6f) + 0.5f;
 
-		darkGrayImage[(y * width) + x] = static_cast< unsigned char >(grayPix);
+		darkGrayImage[pos] = static_cast< unsigned char >(grayPix);
 	}
 }
 
 // Host code
 void darkGray(const int width, const int height, const unsigned char * inputImage, unsigned char * darkGrayImage) {
-	NSTimer kernelTime = NSTimer("darker", false, false);
+	NSTimer kernelTime = NSTimer("kernelDarker", false, false);	
+	NSTimer allocationTime = NSTimer("allocationDarker", false, false);
 	size_t sizeInputImage = width * height * 3 * sizeof(unsigned char);
 	size_t sizedarkGrayImage = width * height * sizeof(unsigned char);
 
 	// allocate images in device memory
 	unsigned char *inputImageDevice, *darkGrayImageDevice;
+
+	allocationTime.start();
 	cudaMalloc((void **) &inputImageDevice, sizeInputImage);
 	cudaMalloc((void **) &darkGrayImageDevice, sizedarkGrayImage);
-
+	allocationTime.stop();
 	// Copy image from host to device
 	cudaMemcpy(inputImage_device, inputImage, sizeInputImage, cudaMemcpyHostToDevice);
 
 	kernelTime.start();
 	dim3 threadsPerBlock(16, 16);
 	dim3 numBlocks(ceil((float)width / threadsPerBlock.x), ceil((float)height / threadsPerBlock.y));
-	darkGray_kernel<<<numBlocks, threadsPerBlock>>>(width, height, inputImage, darkGrayImage);
+	darkGray_kernel<<<numBlocks, threadsPerBlock>>>(width, height, width*height, inputImage, darkGrayImage);
 	cudaDeviceSynchronize();
 	kernelTime.stop();
 
@@ -60,5 +64,7 @@ void darkGray(const int width, const int height, const unsigned char * inputImag
 	
 	// Time GFLOP/s GB/s
 	cout << fixed << setprecision(6) << kernelTime.getElapsed() << setprecision(3) << " " << (static_cast< long long unsigned int >(width) * height * 7) / 1000000000.0 / kernelTime.getElapsed() << " " << (static_cast< long long unsigned int >(width) * height * (4 * sizeof(unsigned char))) / 1000000000.0 / kernelTime.getElapsed() << endl;
+	cout << fixed << setprecision(6) << allocationTime.getElapsed() << setprecision(3) << endl;
+
 
 }
